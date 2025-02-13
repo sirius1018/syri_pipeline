61d148cbb760a51ff289e1a6cd68030a2fe41cad
sirius <sirius1018@hotmail.com>
2025-02-13 16:06:55 +0800

    20250213

--- run_syri_pipeline.py
+++ run_syri_pipeline.py
@@ -1,6 +1,7 @@
 import subprocess
 import pandas as pd
 from pathlib import Path
+import re
 
 # minimap2 -ax asm5 -t 4 --eqx CGMH058_GCA_013378355.1_genomic.fna SK36_GCA_000014205.1_genomic.fna | samtools sort -O BAM - > CGMH058_SK36.bam
 # syri -c CGMH058_SK36.bam -r CGMH058_GCA_013378355.1_genomic.fna -q SK36_GCA_000014205.1_genomic.fna -F B --prefix CGMH058_SK36
@@ -29,8 +30,8 @@ def cp_genomes(ref_fasta, query_fasta, output):
     subprocess.run(['cp', ref_fasta, query_fasta, output],check=True)
 
 
-def make_genome_file(bam_output, ref_fasta, ref_name, query_fasta, query_name):
-    genome_file =  str(bam_output).strip('.bam') + '_genomes.txt'
+def make_genome_file(genome_file, ref_fasta, ref_name, query_fasta, query_name):
+    
     with open(genome_file, 'w') as f:
         f.write('#file\tname\n')
         f.write(ref_fasta + '\t' + ref_name +'\n')
@@ -40,16 +41,30 @@ def make_genome_file(bam_output, ref_fasta, ref_name, query_fasta, query_name):
     print(f'Genome file created: {genome_file}')
 
 
-def plotsr(syri_out_folder):
+def plotsr(syri_out_folder, genomes_file, output_png_name):
+    syri_out = list(Path(syri_out_folder).glob('*syri.out'))
+    syri_out_png = Path(syri_out_folder).joinpath(f'{output_png_name}_syri.png')
+    if syri_out:
+        syri_out = syri_out[0]
+        cmd = ['plotsr', '--sr', syri_out, '--genomes', genomes_file, '-o', syri_out_png, '-H', '1', '-W', '5']
+        subprocess.run(cmd, check=True)
+        print(f"Plotsr command executed for: {output_png_name}")
+    else:
+        print(f"No syri output file found in: {syri_out_folder}")
+
+
+    # cmd = ['plotsr', '--sr', syri_out, '--genomes', genomes_file, '-o', syri_out_png, '-H', '1', '-W', '5']
     # plotsr --sr CGMH058_SK36syri.out --genomes genomes.txt -o output.png -H 1 -W 5
-    #subprocess.run(['plotsr', '--sr', syri_out, '--genomes', genomes_file, '-o', output.png, '-H', '1', '-W', '5'],check=True)
-    pass
+    # subprocess.run(cmd, check=True)
+
+
 def main(df):
     for i, row in df.iterrows():
-
+        
         align_name = row['bam_output'].strip('.bam')
         Path(align_name).mkdir(parents=True, exist_ok=True)
-        bam_output = Path(align_name).joinpath(row['bam_output'])
+        bam_output = Path(align_name).joinpath(row['bam_output']) # 完整路徑的 bamfile
+        genome_file =  Path(align_name).joinpath('genomes.txt')
 
         cp_genomes(row['ref_fasta'],row['query_fasta'], Path(align_name))
         
@@ -66,22 +81,29 @@ def main(df):
         ################################################
 
 
+        make_genome_file(genome_file, row['ref_fasta'], row['ref_name'], row['query_fasta'], row['query_name'])
+
+
         ################ syri command ##################
         syri_cmd = ["syri", "-c", bam_output, "-r", row['ref_fasta'], "-q", row['query_fasta'], "-F", "B", "--dir", Path(align_name)]
         subprocess.run(syri_cmd, check=True)
         print(f"SyRI command executed for: {row['bam_output']}")
         ################################################
 
-        make_genome_file(bam_output, row['ref_fasta'], row['ref_name'], row['query_fasta'], row['query_name'])
 
-        # break
+
+        ########## plotsr command ######################
+        plotsr(Path(align_name), genome_file, align_name)
+        
+        ################################################
+
+        
         # '''
 
 
 
 if __name__ == '__main__':
-    manifest = 'mainfest_test.txt'
+    manifest = 'mainfest.txt'
     a = check_files(manifest,r'20250211_S.sanguinis_comaprison_genome')
-    # make_genome_file(a)
     main(a)
     # subprocess.run(["cat"],stdin=None)
